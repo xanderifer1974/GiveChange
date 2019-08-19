0deedba886ec17118f02aeb75b332fe5ea0c9f02
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-08-19 15:45:00 -0300

    Criacao da Entidade Troco

---
+++ GiveChange/GiveChange.Domain/Entities/Troco.cs
@@ -0,0 +1,12 @@
+﻿namespace GiveChange.Domain.Entities
+{
+    public class Troco
+    {
+        public int Id { get; private set; }
+        public string NomeProduto { get; set; }
+        public decimal PrecoProduto { get; set; }
+        public decimal ValorPago { get; set; }
+        public decimal ValorTroco { get; set; }
+
+    }
+}
