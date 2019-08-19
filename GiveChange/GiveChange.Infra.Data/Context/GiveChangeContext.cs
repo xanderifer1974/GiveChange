379d937994eb0fc21305e22e845a8d2567c2939c
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-08-19 16:34:06 -0300

    Adicicao das pastas de infra e criacao da classe GiveChangeContext

---
+++ GiveChange/GiveChange.Infra.Data/Context/GiveChangeContext.cs
@@ -0,0 +1,15 @@
+﻿using GiveChange.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GiveChange.Infra.Data.Context
+{
+   public class GiveChangeContext: DbContext
+    {
+        public DbSet<Troco> Trocos { get; set; }
+
+        public GiveChangeContext(DbContextOptions options): base(options)
+        {
+
+        }
+    }
+}
