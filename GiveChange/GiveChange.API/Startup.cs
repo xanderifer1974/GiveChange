f18cdc9896ae771193c86b31af0fdb16c933a656
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-08-19 19:16:58 -0300

    Configurando o acesso ao banco de dados no webapi

--- GiveChange/GiveChange.API/Startup.cs
+++ GiveChange/GiveChange.API/Startup.cs
@@ -1,20 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using GiveChange.Infra.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace GiveChange.API
 {
     public class Startup
     {
+        IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile("config.json");
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var conn = Configuration.GetConnectionString("GiveChangeDB");
+            services.AddDbContext<GiveChangeContext>(option => option.UseLazyLoadingProxies()
+                                .UseMySql(conn, m => m.MigrationsAssembly("GiveChange.Infra.Data")));
+            services.AddMvcCore();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
