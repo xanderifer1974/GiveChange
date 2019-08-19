6e415fbb60577517136769789f13f6777f5e0b50
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-08-19 20:10:45 -0300

    Mapeando a classe Troco

---
+++ GiveChange/GiveChange.Infra.Data/Migrations/20190819224922_InicialBd.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GiveChange.Infra.Data.Migrations
+{
+    public partial class InicialBd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Trocos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    NomeProduto = table.Column<string>(nullable: true),
+                    PrecoProduto = table.Column<decimal>(nullable: false),
+                    ValorPago = table.Column<decimal>(nullable: false),
+                    ValorTroco = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trocos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Trocos");
+        }
+    }
+}
