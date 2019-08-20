55050b473bea6d1f081949ce54c172a5f3c68076
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-08-20 10:28:29 -0300

    Criacao da classe BaseRepositoy na camada de Infra

---
+++ GiveChange/GiveChange.Infra.Data/Repositories/BaseRepository.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GiveChange.Domain.Interfaces.Repositories;
+using GiveChange.Infra.Data.Context;
+
+namespace GiveChange.Infra.Data.Repositories
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
+    {
+        /// <summary>
+        /// Adicionando referência a classe de contexto
+        /// </summary>
+        protected readonly GiveChangeContext GiveChangeContext;
+
+        public BaseRepository(GiveChangeContext giveChangeContext)
+        {
+
+            GiveChangeContext = giveChangeContext;
+
+        }
+
+        public void Adicionar(TEntity entity)
+        {
+            GiveChangeContext.Set<TEntity>().Add(entity);
+            GiveChangeContext.SaveChanges();
+        }
+
+        public void Atualizar(TEntity entity)
+        {
+            GiveChangeContext.Set<TEntity>().Update(entity);
+            GiveChangeContext.SaveChanges();
+
+        }       
+
+        public TEntity ObterPorId(int id)
+        {
+            return GiveChangeContext.Set<TEntity>().Find(id);
+        }
+
+        public IEnumerable<TEntity> ObterTodos()
+        {
+            return GiveChangeContext.Set<TEntity>().ToList();
+        }
+
+        public void Remover(TEntity entity)
+        {
+            GiveChangeContext.Remove(entity);
+            GiveChangeContext.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            GiveChangeContext.Dispose();
+        }   
+    }
+}
