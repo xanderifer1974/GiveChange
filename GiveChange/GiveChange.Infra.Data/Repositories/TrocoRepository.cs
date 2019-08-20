f69c33ce2e9ff22d22821d495bd13aa7e54549b8
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-08-20 10:40:03 -0300

    Criacao da classe TrocoRepository na camada de Infra

---
+++ GiveChange/GiveChange.Infra.Data/Repositories/TrocoRepository.cs
@@ -0,0 +1,13 @@
+﻿using GiveChange.Domain.Entities;
+using GiveChange.Domain.Interfaces.Repositories;
+using GiveChange.Infra.Data.Context;
+
+namespace GiveChange.Infra.Data.Repositories
+{
+    public class TrocoRepository : BaseRepository<Troco>, ITrocoRepository
+    {
+        public TrocoRepository(GiveChangeContext giveChangeContext) : base(giveChangeContext)
+        {
+        }
+    }
+}
