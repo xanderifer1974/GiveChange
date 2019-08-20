b12f89ec24d3730f3d6602e564cbba51aeb4ca38
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-08-20 09:38:56 -0300

    Criando a classe abstrata entidade e fazendo metodos de validacao

---
+++ GiveChange/GiveChange.Domain/Entities/Entidade.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GiveChange.Domain.Entities
+{
+    public abstract class Entidade
+    {
+        private List<string> _mensagensValidacao { get; set; }
+        private List<string> mensagemValidacao
+        {
+
+            get { return _mensagensValidacao ?? (_mensagensValidacao = new List<string>()); }
+
+        }
+
+        protected void LimparMensagemValidacao()
+        {
+            mensagemValidacao.Clear();
+        }
+
+        protected void AdicionarCritica(string mensagem)
+        {
+            mensagemValidacao.Add(mensagem);
+        }
+
+        public abstract void Validate();
+
+        protected bool EhValido
+        {
+            get { return !mensagemValidacao.Any(); }
+        }
+    }
+}
--- GiveChange/GiveChange.Domain/Entities/Troco.cs
+++ GiveChange/GiveChange.Domain/Entities/Troco.cs
@@ -1,6 +1,6 @@
 ﻿namespace GiveChange.Domain.Entities
 {
-    public class Troco
+    public class Troco: Entidade
     {
         public int Id { get; private set; }
         public string NomeProduto { get; set; }
@@ -8,5 +8,32 @@
         public decimal ValorPago { get; set; }
         public decimal ValorTroco { get; set; }
 
+        public override void Validate()
+        {
+            LimparMensagemValidacao();
+            if (string.IsNullOrEmpty(NomeProduto))
+            {
+                AdicionarCritica("Crítica - Nome do Produto deve estar preenchido.");
+            }
+            if (PrecoProduto <= 0)
+            {
+                AdicionarCritica("Crítica - Preço do Produto deve ser maior que 0.");
+            }
+
+            if (ValorPago <=  0)
+            {
+                AdicionarCritica("Crítica - Valor pago deve ser maior que 0.");
+            }
+
+            if (ValorPago <= 0)
+            {
+                AdicionarCritica("Crítica - Valor pago deve ser maior que 0.");
+            }
+
+            if (ValorPago < PrecoProduto)
+            {
+                AdicionarCritica("Crítica - Valor pago não pode ser menor que preço do produto.");
+            }
+        }
     }
 }
