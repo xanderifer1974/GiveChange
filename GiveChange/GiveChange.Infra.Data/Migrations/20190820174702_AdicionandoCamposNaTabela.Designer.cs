b0f7d4cd36d823a345539d8fbd00ab481f4e48a7
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-08-20 14:54:01 -0300

    Atualizacao da tabela Troco com os campos de nota e moeda.

--- GiveChange/GiveChange.Infra.Data/Migrations/20190820174702_AdicionandoCamposNaTabela.Designer.cs
+++ GiveChange/GiveChange.Infra.Data/Migrations/20190820174702_AdicionandoCamposNaTabela.Designer.cs
@@ -2,14 +2,16 @@
 using GiveChange.Infra.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GiveChange.Infra.Data.Migrations
 {
     [DbContext(typeof(GiveChangeContext))]
-    partial class GiveChangeContextModelSnapshot : ModelSnapshot
+    [Migration("20190820174702_AdicionandoCamposNaTabela")]
+    partial class AdicionandoCamposNaTabela
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GiveChange/GiveChange.Infra.Data/Migrations/20190820174702_AdicionandoCamposNaTabela.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GiveChange.Infra.Data.Migrations
+{
+    public partial class AdicionandoCamposNaTabela : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Moeda001",
+                table: "Trocos",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Moeda005",
+                table: "Trocos",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Moeda010",
+                table: "Trocos",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Moeda050",
+                table: "Trocos",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Nota10",
+                table: "Trocos",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Nota100",
+                table: "Trocos",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Nota20",
+                table: "Trocos",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Nota50",
+                table: "Trocos",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Moeda001",
+                table: "Trocos");
+
+            migrationBuilder.DropColumn(
+                name: "Moeda005",
+                table: "Trocos");
+
+            migrationBuilder.DropColumn(
+                name: "Moeda010",
+                table: "Trocos");
+
+            migrationBuilder.DropColumn(
+                name: "Moeda050",
+                table: "Trocos");
+
+            migrationBuilder.DropColumn(
+                name: "Nota10",
+                table: "Trocos");
+
+            migrationBuilder.DropColumn(
+                name: "Nota100",
+                table: "Trocos");
+
+            migrationBuilder.DropColumn(
+                name: "Nota20",
+                table: "Trocos");
+
+            migrationBuilder.DropColumn(
+                name: "Nota50",
+                table: "Trocos");
+        }
+    }
+}
